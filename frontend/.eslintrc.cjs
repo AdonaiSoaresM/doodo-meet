47612059f01d11be6eefde511f8d2e831b798ef2
AdonaiSoaresM <adonaielite@gmail.com>
2024-02-28 19:21:28 -0300

    Eslint

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -4,8 +4,8 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:vue/strongly-recommended",
+    "standard-with-typescript",
+    "plugin:vue/vue3-strongly-recommended",
     "plugin:prettier/recommended",
   ],
   overrides: [
