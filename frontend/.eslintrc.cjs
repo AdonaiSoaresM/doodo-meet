01067ffc0b00d570ef1d877fa7016cd907906e6a
AdonaiSoaresM <adonaielite@gmail.com>
2024-02-28 18:34:07 -0300

    Eslint

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["standard-with-typescript", "plugin:vue/vue3-essential", "@typescript-eslint"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["vue"],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+  },
+};
