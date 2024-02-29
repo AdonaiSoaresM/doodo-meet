4ff1cfe1c2cdb1aba81093e0b2400cf53f09a68c
AdonaiSoaresM <adonaielite@gmail.com>
2024-02-29 00:37:25 -0300

    Tooltip Buttons Header

---
+++ frontend/src/components/home/header-buttons.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex gap-5">
+    <TooltipProvider :delay-duration="100">
+      <Tooltip>
+        <TooltipTrigger>
+          <svgVue color="white" :svg="$icons.mdiAccountMultiplePlus" />
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Encontrar Amigo</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <svgVue color="white" :svg="$icons.mdiMessagePlus" />
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Nova Chamada</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export default defineComponent({
+  components: {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  },
+});
+</script>
--- frontend/src/components/home/header.vue
+++ frontend/src/components/home/header.vue
@@ -1,24 +1,25 @@
 <template>
   <div class="flex items-center justify-between w-full h-20 px-7 bg-teal-950">
-    <Avatar class="w-12 h-12">
-      <AvatarImage src="https://github.com/radix-vuee.png" alt="@radix-vue" />
-      <AvatarFallback><span class="font-medium"> CN </span></AvatarFallback>
-    </Avatar>
-    <div class="flex gap-3">
-      <svgVue color="white" :svg="$icons.mdiAccountMultiplePlus"/>
-      <svgVue color="white" :svg="$icons.mdiMessagePlus"/>
+    <div class="flex items-center gap-4">
+      <Avatar class="w-12 h-12">
+        <AvatarImage src="https://github.com/radix-vuee.png" alt="@radix-vue" />
+        <AvatarFallback><span class="font-medium"> CN </span></AvatarFallback>
+      </Avatar>
+      <span class="font-medium text-white">Juliano Almeida</span>
     </div>
+    <HeaderButtons />
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-
+import HeaderButtons from "./header-buttons.vue";
 export default defineComponent({
   components: {
     Avatar,
     AvatarFallback,
     AvatarImage,
+    HeaderButtons,
   },
 });
 </script>
